a44cf33ff7bea39560e55dea2773ac5b83bdfd8d
averypierce <avery.vankirk@gmail.com>
2024-02-04 12:53:07 -0800

    generate 50k teams

--- teams.sh
+++ teams.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+# Don't use this it is so slow
 for i in {1..1000}
 do
-   ../../pokemon-showdown/pokemon-showdown generate-team | ../../pokemon-showdown/pokemon-showdown json-team >> teams.txt
+   ../../pokemon-showdown/pokemon-showdown generate-team | ../../pokemon-showdown/pokemon-showdown json-team >> $1
 done
